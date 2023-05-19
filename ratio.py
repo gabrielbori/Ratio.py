64beb251b66f8d291502920c43d46932558f3c39
Gabriel Bori <gaboxxz@gmail.com>
2023-05-19 20:27:20 -0300

    add support for multi threat to seed varius torrents

--- ratio.py
+++ ratio.py
@@ -1,34 +1,41 @@
+import threading
 from code.process_torrent import process_torrent
 import argparse
 import json
 import sys
 
+
 def parse_args():
-   """Create the arguments"""
-   parser = argparse.ArgumentParser('\nratio.py -c <configuration-file.json>')
-   parser.add_argument("-c", "--configuration", help="Configuration file")
-   return parser.parse_args()
+    """Create the arguments"""
+    parser = argparse.ArgumentParser('\nratio.py -c <configuration-file.json>')
+    parser.add_argument("-c", "--configuration", help="Configuration file")
+    return parser.parse_args()
+
 
 def load_configuration(configuration_file):
     with open(configuration_file) as f:
         configuration = json.load(f)
 
-    if 'torrent' not in configuration:
+    if len(configuration["torrents"]) == 0:
         return None
 
-    return configuration
+    return configuration["torrents"]
+
+
+def runTorrentTracker(configuration):
+    to = process_torrent(configuration)
+    to.tracker_process()
 
 
 if __name__ == "__main__":
     args = parse_args()
     if args.configuration:
-        configuration = load_configuration(args.configuration)
+        torrentsList = load_configuration(args.configuration)
     else:
         sys.exit()
-   
-    if not configuration:
-        sys.exit()
- 
-    to = process_torrent(configuration)
-    to.tracker_process()
 
+    if not torrentsList:
+        sys.exit()
+
+    for i in range(len(torrentsList)):
+        threading.Thread(target=runTorrentTracker, args=(torrentsList[i],)).start()
